a0adb32fbc3d39fbe28f90dce107ef18ff3bc29f
Sourodeep Saha <sourodeepsaha28@gmail.com>
2026-04-07 02:08:56 +0530

    Employee Dashboard

---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../others/Header'
+import TaskListNumbers from '../others/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
+
+const EmployeeDashboard = () => {
+  return (
+     <div className='p-10 bg-[#1C1C1C] h-screen text-white'>
+      <Header/>
+      <TaskListNumbers/>
+      <TaskList/>
+    </div>
+  )
+}
+
+export default EmployeeDashboard 
